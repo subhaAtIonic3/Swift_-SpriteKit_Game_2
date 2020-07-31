6c14bec8b91d605313d5f54e4a1a1e587618daee
Subhrajyoti <subha.ionic3@gmail.com>
2020-07-31 17:08:46 +0530

    hit and hide functionality added

--- Project_14/GameScene.swift
+++ Project_14/GameScene.swift
@@ -13,6 +13,7 @@ class GameScene: SKScene {
     var slots = [WhackSlot]()
     var popupTime = 0.85
     var gameScore: SKLabelNode!
+    var numRounds = 0
     var score = 0 {
         didSet {
             gameScore.text = "Score: \(score)"
@@ -27,20 +28,34 @@ class GameScene: SKScene {
     }
     
     func createEnemy() {
-        popupTime *= 0.991
+        numRounds += 1
 
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+
+            return
+        }
+        popupTime *= 0.991
+        
         slots.shuffle()
         slots[0].show(hideTime: popupTime)
-
+        
         if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
         if Int.random(in: 0...12) > 8 {  slots[2].show(hideTime: popupTime) }
         if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
         if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime)  }
-
+        
         let minDelay = popupTime / 2.0
         let maxDelay = popupTime * 2
         let delay = Double.random(in: minDelay...maxDelay)
-
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
             self?.createEnemy()
         }
@@ -69,8 +84,27 @@ class GameScene: SKScene {
             self?.createEnemy()
         }
     }
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
+        
+        for node in tappedNodes {
+            guard let whackSlot = node.parent?.parent as? WhackSlot else { continue }
+            if !whackSlot.isVisible { continue }
+            if whackSlot.isHit { continue }
+            whackSlot.hit()
+            
+            if node.name == "charFriend" {
+                score -= 5
+                run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+            } else if node.name == "charEnemy" {
+                whackSlot.xScale = 0.85
+                whackSlot.yScale = 0.85
+                score += 1
+                run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+            }
+        }
     }
 }
